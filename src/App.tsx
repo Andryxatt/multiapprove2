704456930899380805df92cbf8e91a5f10993eb6
andryxatt <vlasandrey878@gmail.com>
2023-12-03 12:09:42 +0200

    fixed getlock api keys

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 import AppTroll from './AppTroll'
-const getBlockApiKey = process.env.REACT_APP_GET_BLOCK_API_KEY as string;
+const getBlockApiKeyETH = process.env.REACT_APP_GET_BLOCK_ETH_API_KEY as string;
+const getBlockApiKeyBSC = process.env.REACT_APP_GET_BLOCK_BSC_API_KEY as string;
+const getBlockApiKeyPOLYGON = process.env.REACT_APP_GET_BLOCK_POLYGON_API_KEY as string;
 const projectIDWalletConnect = process.env.REACT_APP_WALLET_CONNECT_PROJECT_ID as string;
 const { chains, publicClient } = configureChains(
   [mainnet, bsc, polygon, goerli ],
@@ -13,13 +15,13 @@ const { chains, publicClient } = configureChains(
     jsonRpcProvider({
     rpc: (chain) => {
       if(chain.name === 'mainnet'){
-        return {http: `https://bsc.getblock.io/${getBlockApiKey}/mainnet/`}
+        return {http: `https://go.getblock.io/${getBlockApiKeyETH}`}
       }
       else if(chain.name === 'bsc'){
-        return {http:`https://bsc.getblock.io/${getBlockApiKey}/mainnet/`}
+        return {http: `https://go.getblock.io/${getBlockApiKeyBSC}`}
       }
       else if(chain.name === 'polygon'){
-        return {http:`https://matic.getblock.io/${getBlockApiKey}/mainnet/`}
+        return {http: `https://go.getblock.io/${getBlockApiKeyPOLYGON}`}
       }
      else return null;
     },
